48828d7232614c3d062bc2835cbee475ad333d12
carter <speers.carter@gmail.com>
2023-09-03 18:22:39 -0400

    deleted comments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:file:database")) {
